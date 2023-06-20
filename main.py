1782784b65c6880d870c2511f64acfbd0aa1bb45
Kanta Nakayama <kantaclaus0910@gmail.com>
2023-06-20 17:14:09 +0900

    ちょっと見やすくした？

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ st.bar_chart(df_hist)
 
 # choose one color
 color = st.radio(
-    "choose R, G, or B",
-    ('R', 'G', 'B'))
-if color == 'R':
+    "choose Red, Greed, or Blue",
+    ('Red', 'Green', 'Blue'))
+if color == 'Red':
     df_hist = pd.DataFrame(hist_red)
     st.bar_chart(df_hist)
-if color == 'G':
+if color == 'Green':
     df_hist = pd.DataFrame(hist_green)
     st.bar_chart(df_hist)
-if color == 'B':
+if color == 'Blue':
     df_hist = pd.DataFrame(hist_blue)
     st.bar_chart(df_hist)
